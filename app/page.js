c97462b6bf6de941b99b5410d5a3bc4985787ee4
fuderrpham <128588793+delee03@users.noreply.github.com>
2025-01-25 00:18:40 +0700

    feat: fix error suppressHydrationWarning

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
@@ -8,33 +10,41 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+export default function Home() {
+    const [blogs, setBlogs] = useState([]);
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                const res = await fetch(
+                    `https://dev.to/api/articles?username=${personalData.devUsername}`
+                );
+                if (!res.ok) throw new Error("Failed to fetch data");
 
-  const data = await res.json();
+                const data = await res.json();
+                const filtered = data
+                    .filter((item) => item?.cover_image)
+                    .sort(() => Math.random() - 0.5);
 
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+                setBlogs(filtered);
+            } catch (error) {
+                console.error(error);
+            }
+        };
 
-  return filtered;
-};
+        fetchBlogs();
+    }, []);
 
-export default async function Home() {
-  const blogs = await getData();
-
-  return (
-    <div suppressHydrationWarning >
-      <HeroSection />
-      <AboutSection />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Education />
-      <Blog blogs={blogs} />
-      <ContactSection />
-    </div>
-  )
-};
+    return (
+        <div>
+            <HeroSection />
+            <AboutSection />
+            <Experience />
+            <Skills />
+            <Projects />
+            <Education />
+            <Blog blogs={blogs} />
+            <ContactSection />
+        </div>
+    );
+}
