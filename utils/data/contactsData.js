8b6c08b96790c26304eb907aa84b4c384b17dd9e
Eric Pham <phamphat2003vn@gmail.com>
2026-05-08 19:18:45 +0700

    chore(data): update Facebook and X/Twitter handles to ericpham03

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,9 +3,9 @@ export const contactsData = {
     phone: "+84 896 555 358",
     address: "Ho Chi Minh City, Vietnam",
     github: "https://github.com/delee03",
-    facebook: "https://www.facebook.com/fuderrpham03/",
+    facebook: "https://www.facebook.com/ericpham03/",
     linkedIn: "https://www.linkedin.com/in/tienthuanphat/",
-    x: "https://x.com/fuderrpham",
+    x: "https://x.com/ericpham03",
     stackOverflow: "https://stackoverflow.com/users/16301392/fuderr-pham",
     devUsername: "fuderrpham03",
 };
